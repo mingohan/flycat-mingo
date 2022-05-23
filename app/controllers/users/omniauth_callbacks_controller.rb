1d04745a004621a391dac31a0c5d7e3fb7eeb5c9
mingohan <mingohandev@gmail.com>
2022-05-23 21:05:23 +0800

    完成後台切版(尚缺請假,排版系統)_暫時移除第三方登入

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ module Users
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect @user, event: :authentication
       else
-        session['devise.google_data'] = request.env['omniauth.auth']
+        session['devise.google_data'] = request.env['omniauth.auth'].except!(:extra)
         redirect_to new_user_registration_path
       end
     end
@@ -22,7 +22,7 @@ module Users
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
         sign_in_and_redirect @user, event: :authentication
       else
-        session["devise.auth_data"] = request.env["omniauth.auth"].except(:extra)
+        session["devise.auth_data"] = request.env["omniauth.auth"].except!(:extra)
         redirect_to new_user_registration_path, alert: @user.errors.full_messages.join("\n")
       end
     end
